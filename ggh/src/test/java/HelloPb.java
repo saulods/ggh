2cd4f04f24e99bda73d5637742d67315d6217abb
saulo <saulo@192.168.1.100>
2013-03-30 00:36:46 -0300

    Terceiro Commit

---
+++ ggh/src/test/java/HelloPb.java
@@ -0,0 +1,35 @@
+
+
+import javax.faces.bean.ManagedBean;
+import javax.faces.bean.RequestScoped;
+ 
+@ManagedBean
+@RequestScoped
+public class HelloPb
+{
+    private String name = "";
+ 
+    public String getName()
+    {
+        return name;
+    }
+ 
+    public void setName(final String name)
+    {
+        this.name = name;
+    }
+ 
+    /**
+     * Say hello + name that was entered.
+     * 
+     * @return
+     */
+    public String getHello()
+    {
+        if ( name == null || name.length() < 1 )
+        {
+            return null;
+        }
+        return "Hello " + name;
+    }
+}
